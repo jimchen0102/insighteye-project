390e5bea50415335d787df321c22e2224ebcfec0
jim.chen <azx011014@gmail.com>
2023-12-13 23:19:18 +0800

    [adjust]: remove tr and td props

--- src/components/QTable.vue
+++ src/components/QTable.vue
@@ -12,35 +12,35 @@
     @update:selected="(row) => $emit('update:selected', row)"
   >
     <template v-slot:body="props">
-      <q-tr :props="props">
+      <q-tr>
         <q-td>
           <q-checkbox v-model="props.selected" />
         </q-td>
-        <q-td key="name" :props="props">
+        <q-td key="name">
           {{ props.row.name }}
           <q-tooltip>
             {{ props.row.name }}
           </q-tooltip>
         </q-td>
-        <q-td key="cellphone" :props="props">
+        <q-td key="cellphone">
           {{ props.row.cellphone }}
           <q-tooltip>
             {{ props.row.cellphone }}
           </q-tooltip>
         </q-td>
-        <q-td key="email" :props="props">
+        <q-td key="email">
           {{ props.row.email }}
           <q-tooltip>
             {{ props.row.email }}
           </q-tooltip>
         </q-td>
-        <q-td key="gender" :props="props">
+        <q-td key="gender">
           {{ props.row.gender }}
           <q-tooltip>
             {{ props.row.gender }}
           </q-tooltip>
         </q-td>
-        <q-td key="birthday" :props="props">
+        <q-td key="birthday">
           {{ props.row.birthday }}
           <q-tooltip>
             {{ props.row.birthday }}
